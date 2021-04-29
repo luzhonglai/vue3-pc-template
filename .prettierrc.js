b1f6ec388e23c607fa29bfc12c8d8e48c957d7f9
ZhongLaiLu <1342341673@qq.com>
2021-04-29 18:33:21 +0800

    feat: 添加mock、eslint、公共文件

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+/*
+ * @Descripttion:
+ * @repository: https://github.com/luzhonglai
+ * @Author: ZhongLai Lu
+ * @Date: 2021-04-29 17:29:03
+ * @LastEditors: Zhonglai Lu
+ * @LastEditTime: 2021-04-29 17:44:34
+ */
+module.exports = {
+  printWidth: 120, // 换行字符串阈值
+
+  tabWidth: 2, // 设置工具每一个水平缩进的空格数
+
+  singleQuote: true, // 用单引号
+
+  semi: false, // 句末是否加分号
+
+  trailingComma: 'none', // 最后一个对象元素加逗号
+
+  bracketSpacing: true, // 对象，数组加空格
+
+  jsxBracketSameLine: true, // jsx > 是否另起一行
+
+  arrowParens: 'always' // (x) => {} 是否要有小括号
+
+  // parser: 'vue' // 指定使用哪一种解析器
+}
---
+++ mock/handlers.ts
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @repository: https://github.com/luzhonglai
+ * @Author: ZhongLai Lu
+ * @Date: 2021-03-30 17:46:05
+ * @LastEditors: Zhonglai Lu
+ * @LastEditTime: 2021-03-30 17:46:05
+ */
